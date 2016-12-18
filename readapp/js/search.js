8e943ca86c8b896d5e4c22f2917a61ac99bf1529
GTuooa <1659256785@qq.com>
2016-12-18 20:41:30 +0800

    0.0

--- readapp/js/search.js
+++ readapp/js/search.js
@@ -20,38 +20,15 @@ $(function () {
     //iscroll
     var myScroll;
     var scroller=document.querySelector("#scroller");
-    var loading=document.querySelector(".loading");
-
-        myScroll = new IScroll('#wrapper', {
-            mouseWheel: true,
-            scrollbars: true,
-            fadeScrollbars:true,
-        });
-
-    myScroll.on("scrollStart",function(){
-        if(myScroll.y==myScroll.maxScrollY){//向下加载
-            loading.style.display="block";
-            setTimeout(function(){
-                for(var i=0;i<1;i++){
-                    var div=document.createElement("div");
-                    div.innerHTML="持续更新中";
-                    div.className="update";
-                    scroller.insertBefore(div,loading);
-                }
-                myScroll.refresh();
-                loading.style.display="none";
-
-            },3000)
-        }
-
-
-    })
-
+    myScroll = new IScroll('#wrapper', {
+        mouseWheel: true,
+        scrollbars: true,
+        fadeScrollbars:true,
+    });
     //swiper
     var mySwiper = new Swiper ('.swiper-container', {
         pagination : '.swiper-pagination',
         autoplay: 5000,//可选选项，自动滑动
         paginationClickable :true,//可点击点点
     })
-
 })//function
